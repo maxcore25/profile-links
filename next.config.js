e33d491ac824c47c959bab336f0b21d1a5f9a3f0
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 06:56:08 +0300

    Next.js Config Set

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: { domains: ['localhost', 'i.ytimg.com'] },
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
