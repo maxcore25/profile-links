4e2c858ccc53146dca869219ac3e81adc01e56b9
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 07:08:46 +0300

    SSG Moved Into The Home Page

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -1,18 +1,5 @@
-import axios from 'axios';
-import { API_URL } from '../../../constants';
-
-const HomePage = () => {
+const Home = ({ links, me }) => {
   return <div>Enter</div>;
 };
 
-export async function getStaticProps(context) {
-  const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
-  const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
-
-  return {
-    props: { links, me },
-    revalidate: 60,
-  };
-}
-
-export default HomePage;
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 import Home from '../app/components/screens/home/Home';
+import axios from 'axios';
+import { API_URL } from '../../../constants';
 
-export default function HomePage() {
-  return <Home />;
+export default function HomePage(props) {
+  return <Home {...props} />;
+}
+
+export async function getStaticProps() {
+  const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
+  const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
+
+  return {
+    props: { links, me },
+    revalidate: 60,
+  };
 }
