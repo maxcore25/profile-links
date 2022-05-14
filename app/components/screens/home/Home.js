0e345bbf838c9e9c4cfb8c5c05efddf95a44ec44
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 07:05:35 +0300

    Axios Installed And SSG Fetching Written

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -1,9 +1,13 @@
-const Home = () => {
+import axios from 'axios';
+import { API_URL } from '../../../constants';
+
+const HomePage = () => {
   return <div>Enter</div>;
 };
 
 export async function getStaticProps(context) {
-  const links = await axios.get();
+  const links = await axios.get(`${API_URL}/links`).then(({ data }) => data);
+  const me = await axios.get(`${API_URL}/me`).then(({ data }) => data);
 
   return {
     props: { links, me },
@@ -11,4 +15,4 @@ export async function getStaticProps(context) {
   };
 }
 
-export default Home;
+export default HomePage;
