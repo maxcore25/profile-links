efacb2587baa3158c5b4cdb816a312a26ced2708
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 06:53:01 +0300

    SSG Started

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -2,4 +2,13 @@ const Home = () => {
   return <div>Enter</div>;
 };
 
+export async function getStaticProps(context) {
+  const links = await axios.get();
+
+  return {
+    props: { links, me },
+    revalidate: 60,
+  };
+}
+
 export default Home;
