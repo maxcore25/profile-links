32547407aea33821395a51f0a0a8c90b52a5074d
maxcore25 <mkorobov2002@mail.ru>
2022-05-14 07:19:09 +0300

    Home Component In Process

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -1,7 +1,13 @@
 import styles from './Home.module.scss';
+import MetaTitle from '../../ui/MetaTitle';
 
 const Home = ({ links, me }) => {
-  return <section className={styles.section}>Enter</section>;
+  return (
+    <section className={styles.section}>
+      <MetaTitle />
+      <div className={styles.container}></div>
+    </section>
+  );
 };
 
 export default Home;
