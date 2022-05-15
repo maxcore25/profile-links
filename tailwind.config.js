f2cd417990d5bf91896c8f62a43dccd6682b4791
maxcore25 <mkorobov2002@mail.ru>
2022-05-15 23:44:50 +0300

    Tailwind Config Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
