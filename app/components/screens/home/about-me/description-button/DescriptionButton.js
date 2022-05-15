145c29415c776fe9afab3644fdeb3442a56d23c1
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 01:07:59 +0300

    useOutside Into DescriptionButton Added

--- app/components/screens/home/about-me/description-button/DescriptionButton.js
+++ app/components/screens/home/about-me/description-button/DescriptionButton.js
@@ -1,9 +1,16 @@
 import useOutside from '@/hooks/useOutside';
+import styles from './DescriptionButton.module.scss';
 
 const DescriptionButton = () => {
   const { ref, isShown, setIsShown } = useOutside();
 
-  return <div>Enter</div>;
+  return (
+    <div className={styles.parent} ref={ref}>
+      <button onClick={() => setIsShown(!isShown)} className={styles.btn}>
+        <span>About me</span>
+      </button>
+    </div>
+  );
 };
 
 export default DescriptionButton;
