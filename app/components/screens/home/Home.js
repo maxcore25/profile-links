470d6372c56b65225d9c83a93114e6ab0991a49d
maxcore25 <mkorobov2002@mail.ru>
2022-05-15 23:38:31 +0300

    Circles Component Added And AnimateBlock Created

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -3,6 +3,7 @@ import MetaTitle from '../../ui/MetaTitle';
 import AboutMe from '../home/about-me/AboutMe';
 import RecentVideos from '../home/recent-videos/RecentVideos';
 import Grid from '../home/grid/Grid';
+import Circles from '../../ui/circles/Circles';
 
 const Home = ({ links, me }) => {
   return (
@@ -13,6 +14,7 @@ const Home = ({ links, me }) => {
         <RecentVideos />
         <Grid links={links} />
       </div>
+      <Circles />
     </section>
   );
 };
---
+++ app/components/ui/animate-block/AnimateBlock.js
@@ -0,0 +1,5 @@
+const AnimateBlock = () => {
+  return <div>Enter</div>;
+};
+
+export default AnimateBlock;
