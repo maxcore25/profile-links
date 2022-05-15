b20270700708134712d1465f6b4e4d59961c5a26
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 00:09:56 +0300

    Tailwind Styles Extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
+const primaryColor = '#ff3850';
+const bgColor = '#111012';
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './app/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: primaryColor,
+        bgColor,
+      },
+      borderRadius: {
+        layout: '1rem',
+      },
+      keyframes: {
+        fade: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        scaleIn: {
+          '0%': { opacity: 0, transform: 'scale(0.9)' },
+          '50%': { opacity: 0.3 },
+          '100%': { opacity: 1, transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        fade: 'fade .5s ease-in-out',
+        scaleIn: 'scaleIn .35s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
