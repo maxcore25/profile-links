1ebec4d99fc387adf6c8c9baa2ae2697306383db
maxcore25 <mkorobov2002@mail.ru>
2022-05-16 08:22:57 +0300

    EmailButton Stylized

--- app/components/screens/home/about-me/email-button/EmailButton.js
+++ app/components/screens/home/about-me/email-button/EmailButton.js
@@ -1,5 +1,26 @@
+import styles from './EmailButton.module.scss';
+import Image from 'next/image';
+import { APP_URL } from '@/app/constants';
+
 const EmailButton = () => {
-  return <div>Enter</div>;
+  return (
+    <div className={styles.button}>
+      <a
+        href='mailto:maxcore2500@gmail.com'
+        target='_blank'
+        rel='noreferrer'
+        title='maxcore2500@gmail.com'>
+        <span>
+          <Image
+            src={`${APP_URL}/icons/email.svg`}
+            alt='Email Icon'
+            width={18}
+            height={18}
+          />
+        </span>
+      </a>
+    </div>
+  );
 };
 
 export default EmailButton;
