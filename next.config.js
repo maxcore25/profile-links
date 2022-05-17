66b9e6ebc1759430e1ad2a5dcbe32b788af8176e
maxcore25 <mkorobov2002@mail.ru>
2022-05-17 11:52:23 +0300

    Next.js Config Modified

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['localhost', 'i.ytimg.com'] },
+  images: {
+    domains: [
+      'localhost',
+      'i.ytimg.com',
+      'avatars.githubusercontent.com',
+      'redlinks.space',
+    ],
+  },
   env: {
     APP_URL: process.env.REACT_APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
