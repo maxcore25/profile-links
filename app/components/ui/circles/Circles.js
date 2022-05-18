10d12c019c46e4d88201e68c2e84e24c3d0941ee
maxcore25 <mkorobov2002@mail.ru>
2022-05-18 22:18:48 +0300

    Circles JSX Written

--- app/components/ui/circles/Circles.js
+++ app/components/ui/circles/Circles.js
@@ -1,5 +1,12 @@
+import styles from './Circles.module.scss';
+
 const Circles = () => {
-  return <div>Enter</div>;
+  return (
+    <div className={styles.circles}>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Circles;
