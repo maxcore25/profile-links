3840964f6bcc63f991a69c25d941abcbc88a44a1
maxcore25 <mkorobov2002@mail.ru>
2022-05-18 23:15:07 +0300

    Parsing Function And Get Title Variable Written

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -1,6 +1,20 @@
+const getString = (initialText, start, end) => {
+  const from = initialText.indexOf(start);
+  const to = initialText.indexOf(end);
+
+  return initialText.substring(from, to).replace(start, '');
+};
+
 export default async function handler(req, res) {
   const data = await fetch('https://www.youtube.com/c/Freecodecamp/videos');
+  const result = await data.text();
 
-  const result = [];
-  res.status(200).json(result);
+  const title = getString(
+    result,
+    '"title":{"runs":[{"text":"',
+    '"}],"accessibility":'
+  );
+
+  //   const result = [];
+  res.status(200).json(title);
 }
