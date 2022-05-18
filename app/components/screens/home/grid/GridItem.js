cc85032e01519caeb68868c0856583d2324f1e24
maxcore25 <mkorobov2002@mail.ru>
2022-05-18 20:27:53 +0300

    GridItem Gradient Block Added

--- app/components/screens/home/grid/GridItem.js
+++ app/components/screens/home/grid/GridItem.js
@@ -1,5 +1,23 @@
+import styles from './Grid.module.scss';
+
 const GridItem = ({ item }) => {
-  return <a href={item.link} rel='noreferrer' target='_blank'></a>;
+  return (
+    <a
+      href={item.link}
+      rel='noreferrer'
+      target='_blank'
+      className={styles.item}>
+      <div
+        className={styles.gradient}
+        style={{
+          background: `linear-gradient(to right, ${item.gradient.from} 0%, ${item.gradient.to} 51%, ${item.gradient.from} 100%)`,
+        }}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={item.icon.path} alt='' width={item.icon.width} />
+      </div>
+      <div className={styles.title}>{item.title}</div>
+    </a>
+  );
 };
 
 export default GridItem;
