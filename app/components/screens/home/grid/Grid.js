517bcf96beec80c2c09ec94c98cba70cdc381840
maxcore25 <mkorobov2002@mail.ru>
2022-05-18 20:57:54 +0300

    Grid JSX Written

--- app/components/screens/home/grid/Grid.js
+++ app/components/screens/home/grid/Grid.js
@@ -1,5 +1,14 @@
+import styles from './Grid.module.scss';
+import GridItem from './GridItem';
+
 const Grid = ({ links }) => {
-  return <div>Enter</div>;
+  return (
+    <div className={styles.grid}>
+      {links.map(link => (
+        <GridItem key={link._id} item={link} />
+      ))}
+    </div>
+  );
 };
 
 export default Grid;
