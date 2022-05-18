c56821e7a4066ce09eccc67addde04ec6e51c2c8
maxcore25 <mkorobov2002@mail.ru>
2022-05-18 20:15:13 +0300

    Parse HTML Function Added

--- app/components/screens/home/about-me/description-button/DescriptionButton.js
+++ app/components/screens/home/about-me/description-button/DescriptionButton.js
@@ -1,6 +1,7 @@
 import { useOutside } from '@/hooks/useOutside';
 import styles from './DescriptionButton.module.scss';
 import cn from 'classnames';
+import parse from 'html-react-parser';
 
 const DescriptionButton = ({ description }) => {
   const { ref, isShown, setIsShown } = useOutside(false);
@@ -12,7 +13,7 @@ const DescriptionButton = ({ description }) => {
         className={cn({ [styles.active]: isShown })}>
         <span>About me</span>
       </button>
-      {isShown && <article>{description}</article>}
+      {isShown && <article>{parse(description)}</article>}
     </div>
   );
 };
