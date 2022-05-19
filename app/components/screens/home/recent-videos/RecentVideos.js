8be015ce0349958e31eebadcca7c9a04bc6091fa
maxcore25 <mkorobov2002@mail.ru>
2022-05-19 11:03:54 +0300

    useQuery Hook Added

--- app/components/screens/home/recent-videos/RecentVideos.js
+++ app/components/screens/home/recent-videos/RecentVideos.js
@@ -1,7 +1,27 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import styles from './RecentVideos.module.scss';
+import VideoItem from './VideoItem';
 
 const RecentVideos = () => {
-  return <div>Enter</div>;
+  const { data, isLoading } = useQuery(
+    'get recent videos',
+    () => axios.get('/api/youtube'),
+    {
+      select: ({ data }) => data,
+    }
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.heading}>New Videos:</div>
+      {isLoading
+        ? 'Loading...'
+        : data?.length
+        ? data.map(video => <VideoItem key={video._id} item={video} />)
+        : 'No Videos'}
+    </div>
+  );
 };
 
 export default RecentVideos;
