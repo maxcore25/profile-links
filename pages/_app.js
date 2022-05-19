d1595377fd1b51591aeec184cbde7079ec42c7dd
maxcore25 <mkorobov2002@mail.ru>
2022-05-19 11:09:14 +0300

    React Query Context Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../app/assets/styles/globals.scss';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
